a8e5e12e2290e8d4620a1f063358ae40eab5391f
Maryen <voujum@gmail.com>
2023-03-15 17:21:08 +0100

    Creted shopping cart link, context and dropdown

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>
-}
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        {/* In the React Router library, the index (short for index={}) prop is used to indicate the default child route that should be rendered when the parent route is matched. */}
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
