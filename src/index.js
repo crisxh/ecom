771c23a3bae5b7561b1fef86a357f09c61e71edd
Maryen <voujum@gmail.com>
2023-03-19 15:43:00 +0100

    Added persist | storage

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.scss';
 import App from './App';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -13,11 +14,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-              <App />
-      </BrowserRouter>
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
 
