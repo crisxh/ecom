e632b36d9b37f68ee5a996f8d0844082a39a9529
Maryen <voujum@gmail.com>
2023-03-20 11:36:22 +0100

    Added spinner to shop and categories

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectCategoriesIsLoading } from '../../store/categories/category.selector'; 
 
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../spinner/spinner.component";
+
 import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
+  const isLoading = useSelector(selectCategoriesIsLoading);
+
   return (
     <div className="category-preview-container">
       <h2>
         <Link className="title" to={title}>{title.toUpperCase()}</Link>
       </h2>
-      <div className="preview">
-        {
-          products.filter((_, indx) => indx < 4)
-                  .map(( product ) => (
-                    <ProductCard key={ product.id } product={ product }  />
-                  ))
-        }
-      </div>
+
+      {isLoading ? (<Spinner />) : (
+        <div className="preview">
+          {products.filter((_, indx) => indx < 4)
+                    .map(( product ) => (
+                      <ProductCard key={ product.id } product={ product }  />
+                    ))}
+        </div>
+      )
+
+      }
     </div>
   );
 };
