7a4fe4fb7187a9e66a81fc19edca6b90001ae329
Maryen <voujum@gmail.com>
2023-03-26 20:23:44 +0200

    Typed payment form component

--- src/components/payment-form/payment-form.component.tsx
+++ src/components/payment-form/payment-form.component.tsx
@@ -7,6 +7,9 @@ import { selectCurrentUser } from "../../store/user/user.selector";
 
 import { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { PaymentFromContainer, FormContainer, PaymentButton } from "./payment-form.styles";
+import { StripeCardElement } from "@stripe/stripe-js";
+
+const ifValidCardElement = (card: StripeCardElement | null): card is StripeCardElement => card !== null;
 
 const PaymentForm = () => {
   const stripe = useStripe();
@@ -15,7 +18,7 @@ const PaymentForm = () => {
   const currentUser = useSelector(selectCurrentUser);
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
-  const paymentHandler = async (e) => {
+  const paymentHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
@@ -33,9 +36,13 @@ const PaymentForm = () => {
 
     const clientSecret = response.paymentIntent.client_secret;
 
+    const cardDetails = elements.getElement(CardElement);
+
+    if (!ifValidCardElement(cardDetails)) return;
+
     const paymentResult = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
-        card: elements.getElement(CardElement),
+        card: cardDetails,
         billing_details: {
           name: currentUser ? currentUser.displayName : 'Mare CLI',
         },
