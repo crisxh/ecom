a303fac5896e8e529fe92237adfedda292eff213
Maryen <voujum@gmail.com>
2023-03-26 11:19:10 +0200

    Typed cart

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,20 +1,22 @@
 import { createSelector } from "reselect";
+import { CartState } from "./cart.reducer";
+import { CartItem } from "./cart.types";
 
-const selectCartReducer = state => state.cart;
+const selectCartReducer = (state): CartState => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCartReducer],
-  (cart) => cart.cartItems
+  (cart): CartItem[] => cart.cartItems
 );
 
 export const selectIsCartOpen = createSelector(
   [selectCartReducer],
-  (cart) => cart.isCartOpen
+  (cart): boolean => cart.isCartOpen
 );
 
 export const selectCartCount = createSelector(
   [selectCartItems],
-  (cartItems) => cartItems.reduce(
+  (cartItems): number => cartItems.reduce(
     (total, currItem) => total + currItem.quantity,
     0
   )
@@ -22,7 +24,7 @@ export const selectCartCount = createSelector(
 
 export const selectCartTotal = createSelector(
   [selectCartItems],
-  (cartItems) => cartItems.reduce(
+  (cartItems): number => cartItems.reduce(
     (cartTotal, currItem) => cartTotal + currItem.price * currItem.quantity,
     0
   )
