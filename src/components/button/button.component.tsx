a2ba282450cd2514693c899e1f5ea30db0b197ca
Maryen <voujum@gmail.com>
2023-03-26 17:16:31 +0200

    Typed button component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,21 +1,25 @@
-import React from "react";
-
+import { FC, ButtonHTMLAttributes } from "react";
 import { BaseButton, GoogleSignInButton, InvertedButton, ButtonSpinner } from './button.styles';
 
-export const BUTTON_TYPE_CLASSES = {
-  base: 'base',
-  google: 'google-sign-in',
-  inverted: 'inverted',
+export enum BUTTON_TYPE_CLASSES {
+  base = 'base',
+  google = 'google-sign-in',
+  inverted = 'inverted',
 };
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton =>
   ({
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+export type ButtonProps = {
+  isLoading?: boolean;
+  buttonType?: BUTTON_TYPE_CLASSES;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: FC<ButtonProps> = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
   return (
     <CustomButton disabled={isLoading} {...otherProps}>
